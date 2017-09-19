c0d2f315b827d861920200a0844fd90b7731b8b0
ericpcho <ericpcho@gmail.com>
2017-09-19 12:55:21 -0700

    added runServer function

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(morgan('common'));
 
-const {DATABASE_URL} = require('./config');
-mongoose.connect(DATABASE_URL);
+const {DATABASE_URL, PORT} = require('./config');
+// mongoose.connect(DATABASE_URL);
 
 mongoose.Promise = global.Promise;
 
@@ -33,3 +33,32 @@ Blog.create({
 }
 
 
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// `closeServer` function is here in original code
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
